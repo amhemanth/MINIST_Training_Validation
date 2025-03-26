be80c9dee9cafc08d78f7f0228ce57734861e45b
A.M. Hemanth <amhemanth1999@gmail.com>
2025-03-26 22:06:00 +0530

    updated requirements

--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -2,12 +2,6 @@
 Unit tests for data loading functionality
 """
 
-import os
-import sys
-
-# Add the project root directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
 import torch
 from src.data.mnist_data import get_data_loaders
 
