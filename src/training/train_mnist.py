41fd2cd6a74fb2fb13e0816547dea3273ef4d225
A.M. Hemanth <amhemanth1999@gmail.com>
2025-03-26 21:48:32 +0530

    Added Docs to code

--- src/training/train_mnist.py
+++ src/training/train_mnist.py
@@ -10,13 +10,13 @@ It handles:
     - Command line argument parsing
 """
 
-import argparse
 import os
 import sys
 
 # Add parent directory to Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
+import argparse
 import torch
 import torch.optim as optim
 from src.data.mnist_data import get_data_loaders
