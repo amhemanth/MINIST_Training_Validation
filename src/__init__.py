df058f437fecfad12e6f52e1d89e5b0951788c68
A.M. Hemanth <amhemanth1999@gmail.com>
2025-03-26 22:19:09 +0530

    updated requirements

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,23 @@ MNIST Classification Project
 Version: 1.0.0
 """
 
-# Import statements using absolute imports
-from src.models.mnist_model import Net
-from src.data.mnist_data import get_data_loaders
-from src.utils.training import train, test
+import sys
+print("Python path in src/__init__.py:", sys.path)
+
+try:
+    from src.models.mnist_model import Net
+except ImportError as e:
+    print("Error importing Net:", e)
+
+try:
+    from src.data.mnist_data import get_data_loaders
+except ImportError as e:
+    print("Error importing get_data_loaders:", e)
+
+try:
+    from src.utils.training import train, test
+except ImportError as e:
+    print("Error importing train/test:", e)
 
 __version__ = "1.0.0"
 
